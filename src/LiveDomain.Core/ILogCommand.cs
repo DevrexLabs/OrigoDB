7f32ff09c4f75c9b777d32abc6e142e7689b3338
rofr <none@none.com>
2011-05-30 22:06:45 +0000

    Files needed for alpha release added

---
+++ src/LiveDomain.Core/ILogCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LiveDomain.Core
+{
+    /// <summary>
+    /// Enables commands to be prepared and executed without 
+    /// generic type parameter M and ignoring return value
+    /// </summary>
+    internal interface ILogCommand
+    {
+        /// <summary>
+        /// Prepare and execute the command
+        /// </summary>
+        /// <param name="model"></param>
+        void Redo(Model model);
+    }
+}
---
+++ src/LiveDomain.Core/SnapshotInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LiveDomain.Core
+{
+    class SnapshotInfo
+    {
+        public DateTime Created { get; set; }
+        public string Name { get; set; }
+        public long SizeOnDiscInBytes { get; set; }
+    }
+}

a53e6dcbb9a08fdde3b458d891ec9ecc501909a0
WooCode <none@none.com>
2012-01-12 14:11:08 +0000

    Added InternalsVisibleTo for LiveDomain.Enterprise

--- src/LiveDomain.Core/Properties/AssemblyInfo.cs
+++ src/LiveDomain.Core/Properties/AssemblyInfo.cs
@@ -35,4 +35,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("LiveDomain.Enterprise")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("LiveDomain.Core.Test")]

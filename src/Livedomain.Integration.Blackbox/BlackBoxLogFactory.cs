4a1e56165db04a18ca4babd9b7322725a7120a78
rofr <none@none.com>
2012-03-08 10:44:26 +0000

    Blackbox logging module

---
+++ src/Livedomain.Integration.Blackbox/BlackBoxLogFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using BlackBox;
+using LiveDomain.Core.Logging;
+using System.ComponentModel.Composition;
+
+namespace Livedomain.Integration.Blackbox
+{
+    [Export(typeof(ILogFactory))]
+    public class BlackBoxLogFactory : ILogFactory
+    {
+        readonly LogKernel _kernel;
+
+        public BlackBoxLogFactory()
+        {
+            var config = LogConfiguration.FromConfigSection();
+            
+            if(config == null)
+            {
+                config = new LogConfiguration();
+                var sink = new ConsoleSink();
+                config.Sinks.Add(sink);
+            }
+
+            _kernel = new LogKernel(config);
+
+        }
+
+        public ILog GetLog(Type type)
+        {
+            var logger = _kernel.GetLogger(type);
+            return new BlackBoxLogAdapter(logger);
+        }
+
+        public ILog GetLogForCallingType()
+        {
+            var logger = _kernel.GetLogger();
+            return new BlackBoxLogAdapter(logger);
+        }
+
+        public ILog GetLog(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

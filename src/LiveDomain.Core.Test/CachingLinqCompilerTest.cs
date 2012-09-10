21f3bdac7998409053b6ac53abcc7bdb1de0d659
rofr <robert@devrex.se>
2012-09-10 11:54:58 +0200

    Compiled linq initial checkin
    Evaluate linq queries passed as parameterized strings
    Dynamically compiled assembly is cached
    Supports strongly typed parameters

---
+++ src/LiveDomain.Core.Test/CachingLinqCompilerTest.cs
@@ -0,0 +1,204 @@
+﻿using LiveDomain.Core.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reflection;
+
+namespace LiveDomain.Core.Test
+{
+
+
+    /// <summary>
+    ///This is a test class for CachingLinqCompilerTest and is intended
+    ///to contain all CachingLinqCompilerTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class CachingLinqCompilerTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for CachingLinqCompiler Constructor
+        ///</summary>
+        [TestMethod()]
+        public void CanCreateCompilerInstance()
+        {
+            new CachingLinqCompiler<TestModel>();
+        }
+
+        [TestMethod()]
+        public void CanCompileValidQueries()
+        {
+            int failedQueries = 0;
+            for (int i = 0; i < validQueries.Length;i++)
+            {
+                QueryDefinition queryDefinition = validQueries[i];
+                try
+                {
+                    CachingLinqCompiler<TestModel> target = new CachingLinqCompiler<TestModel>();
+                    var methodInfo = target.GetCompiledQuery(queryDefinition.Query, queryDefinition.Args);
+                }
+                catch (Exception ex)
+                {
+                    failedQueries++;
+                    Console.WriteLine("Query id {0} failed", i);
+                    Console.WriteLine(ex);
+                }
+            }
+
+            Assert.AreEqual(0, failedQueries);
+
+        }
+
+        [TestMethod]
+        public void RepeatedQueryIsCached()
+        {
+            CachingLinqCompiler<TestModel> target = new CachingLinqCompiler<TestModel>();
+            var queryDefinition = validQueries[0];
+            Assert.AreEqual(0, target.CompilerInvocations);
+            target.GetCompiledQuery(queryDefinition.Query, queryDefinition.Args);
+            Assert.AreEqual(1, target.CompilerInvocations);
+            target.GetCompiledQuery(queryDefinition.Query, queryDefinition.Args);
+            Assert.AreEqual(1, target.CompilerInvocations);
+        }
+
+        [TestMethod]
+        public void RepeatedQueryIsCachedWhenParametersDiffer()
+        {
+            CachingLinqCompiler<TestModel> target = new CachingLinqCompiler<TestModel>();
+            var queryDefinition = validQueries[0];
+            Assert.AreEqual(0, target.CompilerInvocations);
+            target.GetCompiledQuery(queryDefinition.Query, new object[] { "H" });
+            Assert.AreEqual(1, target.CompilerInvocations);
+            target.GetCompiledQuery(queryDefinition.Query, new object[]{"R"});
+            Assert.AreEqual(1, target.CompilerInvocations);
+        }
+
+        [TestMethod]
+        public void RepeatedQueryIsCompiledWhenCompilationIsForced()
+        {
+            CachingLinqCompiler<TestModel> target = new CachingLinqCompiler<TestModel>();
+            target.ForceCompilation = true;
+            var queryDefinition = validQueries[0];
+            Assert.AreEqual(0, target.CompilerInvocations);
+            target.GetCompiledQuery(queryDefinition.Query, queryDefinition.Args);
+            Assert.AreEqual(1, target.CompilerInvocations);
+            target.GetCompiledQuery(queryDefinition.Query, queryDefinition.Args);
+            Assert.AreEqual(2, target.CompilerInvocations);
+        }
+
+        [TestMethod]
+        public void CanExecuteQueryWithNewDifferentParameters()
+        {
+            string expected0 = "Homer Simpson";
+            string expected1 = "Robert Friberg";
+
+            var queryDef = validQueries[0];
+            Engine<TestModel> engine = Engine.LoadOrCreate<TestModel>();
+
+            //mutate model using a lambda, Dont ever do this unless testing, modification will not be journaled!
+            engine.Execute(m =>
+            {
+                m.AddCustomer(expected0);
+                return 1;
+            });
+            engine.Execute(m =>
+            {
+                m.AddCustomer(expected1);
+                return 1;
+            });
+           
+            string actual = engine.Execute<TestModel, string>(queryDef.Query, "Ho");
+            Assert.AreEqual(expected0, actual);
+            actual = (string) engine.Execute(queryDef.Query, "Ro");
+            Assert.AreEqual(actual, expected1);
+        }
+
+        [TestMethod]
+        public void CanExecuteValidQueries()
+        {
+            string expected = "Homer Simpson";
+
+            var queryDef = validQueries[0];
+            Engine<TestModel> engine = Engine.LoadOrCreate<TestModel>();
+
+            //mutate model using a lambda, Dont ever do this unless testing, modification will not be journaled!
+            engine.Execute(m => { m.AddCustomer(expected);
+                                          return 1;
+            });
+
+            
+            string actual = engine.Execute<TestModel, string>(queryDef.Query, queryDef.Args);
+            Assert.AreEqual(expected, actual);
+        }
+
+        private QueryDefinition[] validQueries = new QueryDefinition[]
+                                                     {
+                                                         new QueryDefinition
+                                                             {
+                                                                 Query = @"
+(from customer in db.Customers
+where customer.Name.StartsWith(@arg0)
+orderby customer.Name
+select customer.Name)
+.First()",
+                              Args = new object[]{"H"}
+
+                                                             }
+                                                     };
+
+        class QueryDefinition
+        {
+            public string Query;
+            public object[] Args;
+        }
+    }
+}
---
+++ src/LiveDomain.Core/Linq/CachingLinqCompiler.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Microsoft.CSharp;
+
+namespace LiveDomain.Core.Linq
+{
+
+    class CachingLinqCompiler<M> where M : Model
+    {
+
+        /// <summary>
+        /// Bypass cache. Mainly so we can run performance tests
+        /// </summary>
+        public bool ForceCompilation { get; set; }
+
+        public int CompilerInvocations { get; private set; }
+
+        private CodeDomProvider _compiler;
+        private CompilerParameters _compilerParameters;
+
+        public const string WrapperTemplate = @"
+            using System;
+            using System.Linq;
+            using LiveDomain.Core;
+
+            namespace Generated
+            {{
+                class CompiledQuery
+                {{
+
+                    public static object QueryExpr({0} db, {1})
+                    {{
+                        return {2};
+                    }}
+
+                    public static object Execute(Engine<{0}> engine, params object[] args)
+                    {{
+                        
+                        return engine.Execute(model => QueryExpr(model, {3}));
+                    }}
+                }}
+            }}";
+
+
+        Dictionary<string, MethodInfo> _queryCache = new Dictionary<string, MethodInfo>();
+
+        public CachingLinqCompiler()
+        {
+            InitializeCompiler();
+        }
+
+        public MethodInfo GetCompiledQuery(string query, object[] args)
+        {
+            if (ForceCompilation || !_queryCache.ContainsKey(query))
+            {
+                _queryCache[query] = CompileQuery(query, args);
+            }
+            return _queryCache[query];
+        }
+
+        private MethodInfo CompileQuery(string query, object[] args)
+        {
+            string modelTypeName = typeof (M).FullName;
+            string argsDeclaration = BuildArgsDeclaration(args);
+            string argsInvocation = BuildArgsInvocation(args);
+            string code = String.Format(WrapperTemplate, modelTypeName, argsDeclaration, query, argsInvocation);
+#if DEBUG
+            Console.WriteLine("-------------------- Begin generated code ----------------------------------------");
+            Console.WriteLine(code);
+            Console.WriteLine("-------------------- End generated code ----------------------------------------");
+#endif
+            var assembly = CompileCode(code);
+            return assembly.GetType("Generated.CompiledQuery").GetMethod("Execute");
+        }
+
+        private string BuildArgsInvocation(object[] args)
+        {
+            List<string> castExpressions = new List<string>(args.Length);
+            int idx = 0;
+            foreach (object arg in args)
+            {
+                string typeName = arg.GetType().FullName;
+                string castExpression = String.Format("({0})args[{1}]", typeName, idx++);
+                castExpressions.Add(castExpression);
+            }
+            return String.Join(", ", castExpressions);
+        }
+
+
+        private string BuildArgsDeclaration(object[] args)
+        {
+            List<string> declarations = new List<string>(args.Length);
+            int idx = 0;
+            foreach (object arg in args)
+            {
+                string fullTypeName = arg.GetType().FullName;
+                declarations.Add(fullTypeName + " @arg" + idx++ );
+            }
+            return String.Join(", ", declarations);
+        }
+
+
+        private void InitializeCompiler()
+        {
+            // set interface for compilation
+            _compiler = new CSharpCodeProvider();
+            
+
+            // Define parameters to invoke a compiler
+            _compilerParameters = new CompilerParameters();
+
+
+            _compilerParameters.GenerateExecutable = false;
+            _compilerParameters.GenerateInMemory = true;
+            //_compilerParameters.ReferencedAssemblies.Add("System.Core.dll");
+
+            //reference the assembly where the model is defined
+            _compilerParameters.ReferencedAssemblies.Add(typeof(M).Assembly.Location);
+
+            //reference all the assemblies referenced by the model assembly avoiding duplicaties
+            foreach (AssemblyName referencedAssembly in typeof(M).Assembly.GetReferencedAssemblies())
+            {
+                if(!_compilerParameters.ReferencedAssemblies.Contains(referencedAssembly.Name))
+                {
+                    string candidateAssemblyLocation = Assembly.Load(referencedAssembly).Location;
+                    if(!_compilerParameters.ReferencedAssemblies.Contains(candidateAssemblyLocation))
+                    {
+                        _compilerParameters.ReferencedAssemblies.Add(candidateAssemblyLocation);
+                    }
+                }
+
+                
+            }
+
+#if DEBUG
+            _compilerParameters.IncludeDebugInformation = true;
+#endif
+            _compilerParameters.WarningLevel = 3;
+            _compilerParameters.TreatWarningsAsErrors = false;
+            _compilerParameters.CompilerOptions = "/optimize";
+
+            _compilerParameters.TempFiles = new TempFileCollection(".", keepFiles:false);
+        }
+
+        /// <summary>
+        /// Function to compile .Net C#/VB source codes at runtime
+        /// </summary>
+        /// <param name="source">C# or VB source code as a string</param>
+        private Assembly CompileCode(string source)
+        {
+            CompilerInvocations++;
+            string _Errors = String.Empty;
+
+            try
+            {
+                var compilerResults = 
+                    _compiler.CompileAssemblyFromSource(_compilerParameters, source);
+
+                if (compilerResults.Errors.Count > 0)
+                {
+                    // Return compilation errors
+                    foreach (CompilerError compilerError in compilerResults.Errors)
+                    {
+                        _Errors += "Line number " + compilerError.Line +
+                                    ", Error Number: " + compilerError.ErrorNumber +
+                                    ", '" + compilerError.ErrorText + ";\r\n\r\n";
+                    }
+
+                    throw new Exception(_Errors);
+                }
+                return compilerResults.CompiledAssembly;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}

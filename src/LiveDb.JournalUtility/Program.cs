e1a01d61b9db8fe915cb0aa6466c25d11f311638
Chris <chris@woocode.com>
2012-09-10 16:05:10 +0200

    Refactored code and fixed assembly loading.

--- src/LiveDb.JournalUtility/Program.cs
+++ src/LiveDb.JournalUtility/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using LiveDomain.Core.Storage;
@@ -9,6 +10,7 @@ using LiveDomain.Core;
 using LiveDomain.Modules.SqlStorage;
 using System.IO;
 using System.Reflection;
+using TinyIoC;
 
 namespace LiveDb.JournalUtility
 {
@@ -19,15 +21,17 @@ namespace LiveDb.JournalUtility
 
 		static void Main(string[] args)
 		{
-			//file -> file
+			#region Argument tests
+			//file-v0.4 -> file-v0.5
 			args = new[]
-			           {
-			               @"-source=c:\livedb\freedb.001", 
-			               @"-destination=c:\livedb\freedb.003", 
-			               "-source-type=file-v0.4", 
-			               "-destination-type=file", 
-			               @"-assembly=C:\temp\FreeDb\FreeDb.Core\bin\Debug\FreeDb.Core.dll"
-			           };
+				{
+					@"--source=C:\db\freedb.1",
+					@"--destination=C:\db\freedb.1.3",
+					"--source-type=file-v0.4",
+					"--destination-type=file-v0.5",
+					@"--assembly=C:\Dropbox\Shared Work\FreeDb\FreeDb.Core\bin\Debug\FreeDb.Core.dll"
+				};
+
 
 			//file-v0.4 -> file
 			//args = new[]
@@ -71,14 +75,21 @@ namespace LiveDb.JournalUtility
 			//               @"-source-snapshots=c:\livedb\freedb.ss.001", 
 			//               @"-assembly=C:\temp\FreeDb\FreeDb.Core\bin\Debug\FreeDb.Core.dll"
 			//           };
+			#endregion
 
-			var parsedArgs = ParseArgs(args);
+			// Parse and validate arguments
+			var arguments = new Arguments();
+			if (!CommandLine.CommandLineParser.Default.ParseArguments(args, arguments))
+				throw new Exception("Arguments");
+			arguments.Validate();
+
+			// Load assembly and handle assembly resolve
 			AppDomain.CurrentDomain.AssemblyResolve += CurrentDomainAssemblyResolve;
-			LoadAssembly(parsedArgs["assembly"]);
+			LoadAssembly(arguments.Assembly);
 
-			Store destination = CreateStore(parsedArgs["destination"], parsedArgs["destination-type"],
-			                                parsedArgs["destination-snapshots"]);
-			IterateSource(parsedArgs["source"], parsedArgs["source-type"], parsedArgs["source-snapshots"], destination);
+			// Do the thing we do!
+			var destination = CreateStore(arguments.Destination, arguments.DestinationType, arguments.DestinationSnapshots);
+			IterateSource(arguments.Source, arguments.SourceType, arguments.SourceSnapshots, destination);
 		}
 
 		static Assembly CurrentDomainAssemblyResolve(object sender, ResolveEventArgs args)
@@ -91,60 +102,49 @@ namespace LiveDb.JournalUtility
 
 		static void LoadAssembly(string assemblyFile)
 		{
-			var path = Path.GetPathRoot(assemblyFile);
-			var files = Directory.GetFiles(path, "*.dll");
-
-			foreach (var assembly in files.Select(file => Assembly.LoadFrom(path + file)))
-			{
-				_assemblies[assembly.FullName] = assembly;
-			}
+			var assembly = Assembly.LoadFrom(assemblyFile);
+			_assemblies[assembly.FullName] = assembly;
 		}
 
 		static void IterateSource(string location, string sourceType, string snapshotLocation, Store destination)
-		{
+		{		
 			var writer = destination.CreateJournalWriter(0);
+			var journal = GetJournalEntries(location, sourceType, snapshotLocation);
+			initialModel = LoadSnapshot(Path.Combine(snapshotLocation, "000000000.snapshot"));
+			destination.Create(initialModel);
 
-			if (sourceType == "file-v0.4")
+			foreach (var journalEntry in journal)
 			{
-				initialModel = LoadSnapshot(Path.Combine(location, "000000000.snapshot"));
-				destination.Create(initialModel);
-				foreach (var journalEntry in ReadJournal_v4(location))
-				{
-					Console.WriteLine(journalEntry.Id);
-					writer.Write(journalEntry);
-				}
+				Console.WriteLine(journalEntry.Id);
+				writer.Write(journalEntry);
 			}
-			else if (sourceType == "file")
-			{
-				var config = new EngineConfiguration(location);
-				config.SnapshotLocation = snapshotLocation;
-				var store = new FileStore(config);
-				store.Load();
-				initialModel = LoadSnapshot(Path.Combine(config.SnapshotLocation, "000000000.snapshot"));
-				destination.Create(initialModel);
-				foreach (var journalEntry in store.GetJournalEntries())
-				{
-					Console.WriteLine(journalEntry.Id);
-					writer.Write(journalEntry);
-				}
-			}
-			else if (sourceType == "sql")
-			{
-				var config = new SqlEngineConfiguration(location);
-				config.SnapshotLocation = snapshotLocation;
-				var store = new SqlStore(config);
-				store.Load();
-				initialModel = LoadSnapshot(Path.Combine(config.SnapshotLocation, "000000000.snapshot"));
-				destination.Create(initialModel);
-
-				foreach (var journalEntry in store.GetJournalEntries())
-				{
-					Console.WriteLine(journalEntry.Id);
-					writer.Write(journalEntry);
-				}
-			}
-			else throw new ArgumentException("Invalid source type");
 			writer.Close();
+		}
+
+		static IEnumerable<JournalEntry> GetJournalEntries(string location, string sourceType, string snapshotLocation)
+		{
+			EngineConfiguration config;
+			Store store;
+			
+			switch (sourceType)
+			{
+				case "file-v0.4":
+					return ReadJournal_v4(location);
+				case "file-v0.5":
+					config = new EngineConfiguration(location) { SnapshotLocation = snapshotLocation };
+					store = new FileStore(config);
+					break;
+				case "sql":
+					config = new SqlEngineConfiguration(location) { SnapshotLocation = snapshotLocation };
+					store = new SqlStore(config);
+					
+					break;
+				default:
+					throw new ArgumentException("Invalid source type");
+			}
+
+			store.Load();
+			return store.GetJournalEntries();
 		}
 
 		static Model LoadSnapshot(string absolutePath)
@@ -177,7 +177,7 @@ namespace LiveDb.JournalUtility
 
 		static Store CreateStore(string location, string type, string snapshotLocation)
 		{
-			if (type == "file")
+			if (type == "file-v0.5")
 			{
 				var config = new EngineConfiguration(location);
 				config.SnapshotLocation = snapshotLocation;
@@ -190,58 +190,6 @@ namespace LiveDb.JournalUtility
 				return new SqlStore(config);
 			}
 			else throw new ArgumentException("Invalid destination type");
-		}
-
-		static Dictionary<string, string> ParseArgs(string[] args)
-		{
-			Regex regex = new Regex("^-(?<key>.*?)=(?<val>.*)$");
-			var dictionary = new Dictionary<string, string>();
-			foreach (var pair in args)
-			{
-				Match m = regex.Match(pair);
-				if (!m.Success) throw new ArgumentException("bad parameter format");
-				string key = m.Groups["key"].Value;
-				string val = m.Groups["val"].Value;
-				if (dictionary.ContainsKey(key)) throw new ArgumentException("Duplicate parameter");
-				dictionary[key] = val;
-			}
-
-			//Validate
-			var required = new[] {"source", "destination", "assembly"};
-			var allowed = new[]
-				{
-					"source", "source-type", "destination", "destination-type", "source-snapshots", "destination-snapshots", "assembly"
-				};
-			Validate(dictionary.Keys.ToArray(), allowed, required);
-
-			//set defaults
-			if (!dictionary.ContainsKey("source-type")) dictionary["source-type"] = "file";
-			if (!dictionary.ContainsKey("destination-type")) dictionary["destination-type"] = "file";
-			if (!dictionary.ContainsKey("destination-snapshots"))
-				dictionary["destination-snapshots"] = dictionary["destination"];
-			if (!dictionary.ContainsKey("source-snapshots")) dictionary["source-snapshots"] = dictionary["source"];
-
-			if (dictionary["source-type"] == dictionary["destination-type"])
-				throw new ArgumentException("source and destination types must be different");
-
-			if (dictionary["source-type"] == "file-v0.4" && dictionary["destination-type"] == "file" &&
-			    dictionary["source"] == dictionary["destination"])
-				throw new ArgumentException(
-					"source and destination cannot be the same (in-place upgrade not supported at this time)");
-			return dictionary;
-		}
-
-		static void Validate(string[] keysGiven, string[] keysAllowed, string[] keysRequired)
-		{
-			foreach (string key in keysGiven)
-			{
-				if (!keysAllowed.Contains(key)) throw new ArgumentException("Invalid key:" + key);
-			}
-
-			foreach (var key in keysRequired)
-			{
-				if (!keysGiven.Contains(key)) throw new ArgumentException("key required: " + key);
-			}
 		}
 	}
 }

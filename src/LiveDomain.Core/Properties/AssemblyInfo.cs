89e58d7e5177632632c299609afe3ef07883015b
rofr <robert@devrex.se>
2012-10-22 17:01:10 +0200

    release 0.8.0

--- src/LiveDomain.Core/Properties/AssemblyInfo.cs
+++ src/LiveDomain.Core/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("LiveDomain.Enterprise")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("LiveDomain.Core.Test")]

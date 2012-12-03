b732fa3240e80f41a50ef75be643506b4fc1e4e8
rofr <robert@devrex.se>
2012-12-03 17:24:24 +0100

    initial benchmarking utility checking - 12000 TPS!

---
+++ src/LiveDomain.Benchmark/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LiveDomain.Core;
+using LiveDomain.Core.Configuration;
+using System.Diagnostics;
+
+namespace LiveDomain.Benchmark
+{
+
+    [Serializable]
+    public class BenchmarkModel : Model
+    {
+        public int CommandsExecuted;
+        public long BytesWritten;
+    }
+
+    [Serializable]
+    public class Message : Command<BenchmarkModel>
+    {
+        public readonly byte[] Payload;
+
+        public Message(int size)
+        {
+            Payload = new byte[size];
+        }
+
+        protected override void Execute(BenchmarkModel model)
+        {
+            model.CommandsExecuted++;
+            model.BytesWritten += Payload.Length;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int iterations = 20000;
+            const int messageSize = 3000;
+            var config = new EngineConfiguration(Guid.NewGuid().ToString());
+            config.KernelMode = KernelMode.Pessimistic;
+            var engine = Engine.LoadOrCreate<BenchmarkModel>(config);
+            TimeThis(iterations, () => engine.Execute(new Message(messageSize)));
+            engine.Close();
+            Console.ReadLine();
+        }
+
+        static void TimeThis(int iterations, Action action)
+        {
+            Console.WriteLine("{0} iterations..", iterations);
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int i = 0; i < iterations; i++)
+            {
+                action.Invoke();
+            }
+            stopWatch.Stop();
+            long millisElapsed = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine("Elapsed: {0} ms", millisElapsed);
+            long invocationsPerSecond = iterations*1000/millisElapsed;
+            Console.WriteLine("Invocations per second: {0}", invocationsPerSecond);
+        }
+
+        static void TimeThese(int iterations, params Action[] actions)
+        {
+            
+        }
+    }
+}

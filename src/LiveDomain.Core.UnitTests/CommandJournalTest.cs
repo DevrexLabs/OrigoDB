cfaaf4d4601dc23609970306b748e6b794cce2df
rofr <robert@devrex.se>
2012-12-07 21:27:02 +0100

    migrating tests to nunit

--- src/LiveDomain.Core.UnitTests/CommandJournalTest.cs
+++ src/LiveDomain.Core.UnitTests/CommandJournalTest.cs
@@ -1,72 +1,22 @@
 ﻿using System.Linq;
 using LiveDomain.Core;
 using LiveDomain.Core.Storage;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using LiveDomain.Core.Journaling;
+using NUnit.Framework;
 
 namespace LiveDomain.Core.Test
 {
-    
-    
+
+
     /// <summary>
     ///This is a test class for CommandJournalTest and is intended
     ///to contain all CommandJournalTest Unit Tests
     ///</summary>
-    [TestClass()]
+    [TestFixture]
     public class CommandJournalTest
     {
-
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        // 
-        //You can use the following additional attributes as you write your tests:
-        //
-        //Use ClassInitialize to run code before running the first test in the class
-        //[ClassInitialize()]
-        //public static void MyClassInitialize(TestContext testContext)
-        //{
-        //}
-        //
-        //Use ClassCleanup to run code after all tests in a class have run
-        //[ClassCleanup()]
-        //public static void MyClassCleanup()
-        //{
-        //}
-        //
-        //Use TestInitialize to run code before running each test
-        //[TestInitialize()]
-        //public void MyTestInitialize()
-        //{
-        //}
-        //
-        //Use TestCleanup to run code after each test has run
-        //[TestCleanup()]
-        //public void MyTestCleanup()
-        //{
-        //}
-        //
-        #endregion
-
 
         public class NullStore : IStore
         {
@@ -77,7 +27,7 @@ namespace LiveDomain.Core.Test
             }
             public void VerifyCanLoad()
             {
-                
+
             }
 
             public Model LoadMostRecentSnapshot(out long lastEntryId)
@@ -102,7 +52,7 @@ namespace LiveDomain.Core.Test
 
             public void Load()
             {
-                
+
             }
 
             public bool Exists
@@ -135,7 +85,7 @@ namespace LiveDomain.Core.Test
         {
             private bool isDisposed;
             public List<JournalEntry> Entries { get; private set; }
-        
+
 
             public MemoryJournal()
             {
@@ -165,15 +115,8 @@ namespace LiveDomain.Core.Test
             }
         }
 
-        /// <summary>
-        ///A test for GetAllEntries
-        ///</summary>
-        [TestMethod()]
-        public void RolledBackCommandsAreSkipped()
-        {
-            CommandJournal target = new CommandJournal(new NullStore());
 
-            var testCases = new List<Tuple<List<JournalEntry>, string>>()
+        List<Tuple<List<JournalEntry>, string>> _testCases = new List<Tuple<List<JournalEntry>, string>>()
                                 {
                                     Tuple.Create(GenerateEntries(5, 3), "Intermediate entry rolled back"),
                                     Tuple.Create(GenerateEntries(5, 1), "First entry rolled back"),
@@ -186,67 +129,78 @@ namespace LiveDomain.Core.Test
                                     Tuple.Create(GenerateEntries(100, 9, 10, 11, 48, 62, 63), "Mixed patches of single/multiple entries rolled back"),
                                     Tuple.Create(GenerateEntries(13, 1, 13), "First and last entries rolled back"),
                                 };
-            foreach (Tuple<List<JournalEntry>, string> testCase in testCases)
+
+        [TestCaseSource("_testCases")]
+        public void RolledBackCommandsAreSkipped(Tuple<List<JournalEntry>, string> testCase)
+        {
+            CommandJournal target = new CommandJournal(new NullStore());
+
+            string failureMessage = testCase.Item2;
+            var testEntries = testCase.Item1;
+
+            //Act
+            var actualCommandEntries = target.GetCommandEntries(() => testEntries).ToArray();
+
+            long[] rolledBackIds = testEntries.OfType<JournalEntry<RollbackMarker>>().Select(e => e.Id).ToArray();
+            int expectedNumberOfCommandEntries = testEntries.Count - rolledBackIds.Length * 2;
+
+            //Assert
+            Assert.AreEqual(expectedNumberOfCommandEntries, actualCommandEntries.Length, failureMessage);
+
+            int sequentialId = 1;
+
+            foreach (JournalEntry<Command> journalEntry in actualCommandEntries)
             {
-                string failureMessage = testCase.Item2;
-                var testEntries = testCase.Item1;
-
-                //Act
-                var actualCommandEntries = target.GetCommandEntries(() => testEntries).ToArray();
-                
-                long[] rolledBackIds = testEntries.OfType<JournalEntry<RollbackMarker>>().Select(e => e.Id).ToArray();
-                int expectedNumberOfCommandEntries = testEntries.Count - rolledBackIds.Length * 2;
-
-                //Assert
-                Assert.AreEqual(expectedNumberOfCommandEntries, actualCommandEntries.Length, failureMessage);
-                
-                int sequentialId = 1;
-                
-                foreach (JournalEntry<Command> journalEntry in actualCommandEntries)
+                if (!rolledBackIds.Contains(sequentialId))
                 {
-                    if (!rolledBackIds.Contains(sequentialId))
-                    {
-                        //TODO: Maybe we should return no-op commands instead of skipping rolled back
-                        int expectedId = sequentialId + rolledBackIds.Count(id => id < journalEntry.Id);
-                        Assert.AreEqual(expectedId, journalEntry.Id, failureMessage);
-                    }
-                    sequentialId++;
+                    //TODO: Maybe we should return no-op commands instead of skipping rolled back
+                    int expectedId = sequentialId + rolledBackIds.Count(id => id < journalEntry.Id);
+                    Assert.AreEqual(expectedId, journalEntry.Id, failureMessage);
                 }
+                sequentialId++;
             }
-
         }
 
         private static List<JournalEntry> GenerateEntries(int numEntries, params int[] failedCommandIds)
         {
-            List<JournalEntry> testEntries = new List<JournalEntry>();
+            var testEntries = new List<JournalEntry>();
             for (int i = 1; i <= numEntries; i++)
             {
                 testEntries.Add(new JournalEntry<Command>(i, null));
-                if(failedCommandIds.Contains(i)) testEntries.Add(new JournalEntry<RollbackMarker>(i, null));
+                if (failedCommandIds.Contains(i)) testEntries.Add(new JournalEntry<RollbackMarker>(i, null));
             }
             return testEntries;
         }
 
-
-        /// <summary>
-        ///A test for Rollback
-        ///</summary>
-        [TestMethod()]
+        [Test]
         public void RollbackMarkerIsWrittenOnRollback()
         {
             //Arrange
             MemoryJournal journal = new MemoryJournal();
             CommandJournal target = new CommandJournal(new NullStore(journal));
-            target.Append(new TestCommandWithResult());
-            target.Append(new TestCommandWithoutResult());
-            
+            target.Append(new ACommand());
+            target.Append(new ACommand());
+
             //Act
             target.WriteRollbackMarker();
-            
+
             //Assert
             Assert.AreEqual(3, journal.Entries.Count);
             Assert.AreEqual(1, journal.Entries.OfType<JournalEntry<RollbackMarker>>().Count());
             Assert.IsTrue(journal.Entries.Last() is JournalEntry<RollbackMarker>);
+        }
+
+        private class ACommand : Command
+        {
+            internal override void PrepareStub(Model model)
+            {
+                throw new NotImplementedException();
+            }
+
+            internal override object ExecuteStub(Model model)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }

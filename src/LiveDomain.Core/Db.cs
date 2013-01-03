23a4e9a8f316edf21f289ce55b3f02df27918b06
rofr <robert@devrex.se>
2013-01-03 01:30:27 +0100

    Added parameterless Db.For<T>()

--- src/LiveDomain.Core/Db.cs
+++ src/LiveDomain.Core/Db.cs
@@ -4,6 +4,13 @@ namespace LiveDomain.Core
 {
 	public static class Db
 	{
+
+        public static T For<T>() where T : Model, new()
+        {
+            var instance = Engine.For<T>();
+            return (T)new ModelProxy<T>(instance).GetTransparentProxy();
+        }
+
 		public static T For<T>(string clientIdentifier) where T : Model,new()
 		{
 			var instance = Engine.For<T>(clientIdentifier);

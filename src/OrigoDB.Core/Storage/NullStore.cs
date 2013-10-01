3fdae90bf57ba25bf5de8893ae4d796d5696a43a
rofr <robert@devrex.se>
2013-10-01 03:03:46 +0200

    file cleanup

---
+++ src/OrigoDB.Core/Storage/NullStore.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrigoDB.Core
+{
+    public class NullStore : IStore
+    {
+
+        Model _model;
+
+        public void VerifyCanLoad()
+        {
+
+        }
+
+        public Model LoadMostRecentSnapshot(out long lastEntryId)
+        {
+            lastEntryId = -1;
+            return null;
+        }
+
+        public void WriteSnapshot(Model model, long lastEntryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VerifyCanCreate()
+        {
+        }
+
+        public void Create(Model model)
+        {
+            _model = model;
+        }
+
+        public void Load()
+        {
+            
+        }
+
+        public bool Exists
+        {
+            get { return _model != null; }
+        }
+
+        public IEnumerable<JournalEntry> GetJournalEntries()
+        {
+            return Enumerable.Empty<JournalEntry>();
+        }
+
+        public IEnumerable<JournalEntry> GetJournalEntriesFrom(long entryId)
+        {
+            return Enumerable.Empty<JournalEntry>();
+        }
+
+        public IEnumerable<JournalEntry> GetJournalEntriesBeforeOrAt(DateTime pointInTime)
+        {
+            return Enumerable.Empty<JournalEntry>();
+        }
+
+        public IJournalWriter CreateJournalWriter(long lastEntryId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public System.IO.Stream CreateJournalWriterStream(long firstEntryId = 1)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Model LoadModel()
+        {
+            return _model;
+        }
+    }
+}

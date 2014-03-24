381f7b76653e594f33934818bcd195e4c786b258
rofr <robert@devrex.se>
2014-03-24 07:48:43 +0100

    moved license to root, more tests

--- src/OrigoDB.Core.UnitTests/StoreTests.cs
+++ src/OrigoDB.Core.UnitTests/StoreTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Runtime.InteropServices;
 using NUnit.Framework;
@@ -9,53 +10,58 @@ namespace OrigoDB.Core.Test
     [TestFixture]
     public class StoreTests
     {
+
+        EngineConfiguration _config;
+        private IStore _store;
+
+
+        [SetUp]
+        public void Init()
+        {
+            _config = EngineConfiguration.Create().ForIsolatedTest().ForImmutability();
+            _store = new InMemoryStore(_config);
+            _store.Init();
+            
+        }
+
         [Test]
         public void Create_from_type_creates_journal_entry()
         {
-            var config = EngineConfiguration.Create().ForIsolatedTest();
-            var store = new InMemoryStore(config);
-            store.Init();
 
-            store.Create(typeof(ImmutableModel));
-            var entries = store.GetJournalEntries().ToArray();
+            _store.Create(typeof(ImmutableModel));
+            var entries = _store.GetJournalEntries().ToArray();
             Assert.AreEqual(1, entries.Length);
             var firstEntry = entries[0] as JournalEntry<ModelCreated>;
             Assert.NotNull(firstEntry);
             Assert.AreEqual(firstEntry.Item.Type, typeof(ImmutableModel));
-            Assert.IsFalse(store.Snapshots.Any());
+            Assert.IsFalse(_store.Snapshots.Any());
         }
 
         [Test]
         public void Can_load_from_journal_only()
         {
-            var config = EngineConfiguration.Create().ForIsolatedTest();
-            var store = new InMemoryStore(config);
-            store.Init();
-
-            store.Create(typeof(ImmutableModel));
-            Model model = store.LoadModel();
+            _store.Create(typeof(ImmutableModel));
+            Model model = _store.LoadModel();
             Assert.IsInstanceOf<ImmutableModel>(model);
         }
 
         [Test]
         public void Can_load_from_type()
         {
-            var config = EngineConfiguration.Create().ForIsolatedTest().ForImmutability();
-            var store = new InMemoryStore(config);
-            store.Init();
-
-            var model = (ImmutableModel) store.LoadModel(typeof (ImmutableModel));
-            var engine = new Engine<ImmutableModel>(model, store, config);
+            var model = (ImmutableModel) _store.LoadModel(typeof (ImmutableModel));
+            var engine = new Engine<ImmutableModel>(model, _store, _config);
             engine.Execute(new AppendNumberCommand(53));
             engine.Execute(new AppendNumberCommand(42));
             engine.Dispose();
-            store = new InMemoryStore(config);
-            store.Init();
-            model = (ImmutableModel) store.LoadModel(typeof (ImmutableModel));
+            _store = new InMemoryStore(_config);
+            _store.Init();
+            model = (ImmutableModel) _store.LoadModel(typeof (ImmutableModel));
+            
+            //make sure state is valid after restore
             Assert.AreEqual(53 + 42, model.Numbers().Sum());
+            
 
-            ulong id = 1;
-            var ids = store.GetJournalEntries().Select(je => (int) je.Id).ToArray();
+            var ids = _store.GetJournalEntries().Select(je => (int) je.Id).ToArray();
             Assert.AreEqual(Enumerable.Range(1,2),ids);
 
         }

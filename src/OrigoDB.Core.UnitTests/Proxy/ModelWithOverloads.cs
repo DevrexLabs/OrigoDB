866575b6544043cf21e28ad326f1eb410cefdc60
rofr <robert@devrex.se>
2014-10-21 14:10:11 +0200

    failing test for Proxy overloads

---
+++ src/OrigoDB.Core.UnitTests/Proxy/ModelWithOverloads.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OrigoDB.Core.Test
+{
+    [Serializable]
+    public class ModelWithOverloads : Model
+    {
+        public void Meth()
+        {
+            
+        }
+
+        public void Meth(object state)
+        {
+            
+        }
+
+        public void Meth(params object[] stuff)
+        {
+            
+        }
+
+        public void Meth(ref object state)
+        {
+            
+        }
+
+    }
+}

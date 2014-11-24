7f848e4ca357ba17b1488c1db2be316d6f8eeba7
Patrik Svensson <patrik@patriksvensson.se>
2014-11-24 19:37:08 +0100

    Fixed build script.

--- build.cmd
+++ build.cmd
@@ -2,12 +2,17 @@
  
 :Build
 cls
-  
-   
+     
 if not exist tools\Cake\Cake.exe ( 
   	echo Installing Cake...
- 	nuget.exe install Cake -OutputDirectory tools -ExcludeVersion -NonInteractive
-	 	echo.
+ 	tools\nuget\nuget.exe install Cake -OutputDirectory tools -ExcludeVersion -NonInteractive
+ 	echo.
+)
+
+if not exist tools\NUnit.Runners\tools\nunit-console.exe (
+	echo Installing NUnit runners...
+	tools\nuget\nuget.exe install nunit.runners -OutputDirectory tools -ExcludeVersion -NonInteractive
+	echo.
 )
 			 
 SET BUILDMODE="Release"

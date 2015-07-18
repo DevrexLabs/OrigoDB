00ffe6da55ca468f878ec6453febfe1182212a50
rofr <robert@devrex.se>
2015-07-18 20:14:03 +0200

    add missing file

---
+++ src/OrigoDB.Core.UnitTests/Models/MessageBrokerTests.cs
@@ -0,0 +1,58 @@
+using System;
+using NUnit.Framework;
+using OrigoDB.Core.Models;
+
+namespace OrigoDB.Core.Test
+{
+    [TestFixture]
+    public class MessageBrokerTests
+    {
+        [Test]
+        public void SmokeTest()
+        {
+            const string aQueue = "myqueue";
+            const string aTopic = "mytopic";
+            const string aGreeting = "Hello world!";
+            var aMessage = new TextMessage(aGreeting);
+
+
+            var config = new EngineConfiguration().ForIsolatedTest();
+            var broker = Db.For<MessageBroker>(config);
+            
+            //create/write/read queue
+            broker.CreateQueue(aQueue);
+            broker.Enqueue(aQueue, aMessage);
+            var message = (TextMessage) broker.Dequeue(aQueue);
+            Assert.IsNotNull(message);
+            Assert.AreEqual(aGreeting, message.Body);
+
+            //if queue is empty null is returned
+            message = (TextMessage) broker.Dequeue(aQueue);
+            Assert.IsNull(message);
+
+            broker.CreateTopic(aTopic);
+            
+            //no op, no subscribers
+            broker.Publish(aTopic, aMessage);
+
+            var aSubscriber = Guid.NewGuid();
+            broker.Subscribe(aSubscriber, aTopic);
+
+            broker.Publish(aTopic, aMessage);
+            broker.Publish(aTopic, aMessage);
+
+            var messages = broker.Poll(aSubscriber, aTopic);
+            Assert.AreEqual(messages.Length, 2);
+
+            //Messages are immutable so we should get same instances back!
+            Assert.AreSame(messages[0], aMessage);
+            
+            Guid[] subscribers = broker.GetSubscribers(aTopic);
+            Assert.AreEqual(subscribers.Length, 1);
+            Assert.AreEqual(aSubscriber, subscribers[0]);
+
+            broker.Unsubscribe(aSubscriber, aTopic);
+            Assert.AreEqual(broker.GetSubscribers(aTopic).Length, 0);
+        }
+    }
+}

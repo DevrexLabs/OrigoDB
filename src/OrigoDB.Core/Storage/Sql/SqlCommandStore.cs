b1f83f8f8f7b9f2fe6d69db110e5e7af084b7ecb
rofr <robert@devrex.se>
2015-08-10 15:30:36 +0200

    sql storage skeleton

---
+++ src/OrigoDB.Core/Storage/Sql/SqlCommandStore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OrigoDB.Core.Storage.Sql
+{
+    public class SqlCommandStore : CommandStore
+    {
+
+        public SqlCommandStore(EngineConfiguration config)
+            :base(config)
+        {
+            
+        }
+
+        protected override IEnumerable<JournalEntry> GetJournalEntriesFromImpl(ulong entryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Stream CreateJournalWriterStream(ulong firstEntryId = 1)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IJournalWriter CreateStoreSpecificJournalWriter()
+        {
+            //return base.CreateStoreSpecificJournalWriter();
+            return new SqlJournalWriter(null);
+        }
+    }
+}
---
+++ src/OrigoDB.Core/Storage/Sql/SqlJournalWriter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using OrigoDB.Core.Utilities;
+
+namespace OrigoDB.Core.Storage.Sql
+{
+    
+    public class SqlJournalWriter : IJournalWriter
+    {
+        private readonly Func<JournalEntry, IDbCommand> _commandFactory; 
+
+        public SqlJournalWriter(Func<JournalEntry, IDbCommand> commandFactory)
+        {
+            Ensure.NotNull(commandFactory, "commandFactory");
+            _commandFactory = commandFactory;
+        }
+
+        public void Write(JournalEntry item)
+        {
+            var sqlCommand = _commandFactory.Invoke(item);
+            sqlCommand.ExecuteNonQuery();
+        }
+
+        public void Close()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
